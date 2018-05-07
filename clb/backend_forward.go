f8055daaa22a807a1690fbf18fbe53c56eff1d57
devinyan <devinyan@tencent.com>
2018-05-07 20:01:35 +0800

    创建应用型LB，添加监听器，监听器添加节点成功

---
+++ clb/backend_forward.go
@@ -0,0 +1,135 @@
+package clb
+
+type DescribeForwardLBBackendsArgs struct {
+	LoadBalancerId string `qcloud_arg:"loadBalancerId,required"`
+	Listeners      []string `qcloud_arg:"listeners"`
+	Protocol        int    `qcloud_arg:"protocol"`
+	LoadBalancerPort  int    `qcloud_arg:"loadBalancerPort"`
+}
+
+type DescribeForwardLBBackendsResponse struct {
+	Response
+	Data []ForwardLBBackendsData `json:"data"`
+}
+
+type ForwardLBBackendsData struct {
+	ListenerID       string `json:"listenerId"`
+	LoadBalancerPort int    `json:"loadBalancerPort"`
+	Protocol         int    `json:"protocol"`
+	ProtocolType     string `json:"protocolType"`
+	Rules            []ForwardLBBackendsRule `json:"rules"`
+}
+
+type ForwardLBBackendsRule struct {
+	Domain     string `json:"domain"`
+	LocationID string `json:"locationId"`
+	URL        string `json:"url"`
+	Backends   []ForwardLBBackendsInfo`json:"backends"`
+}
+
+type ForwardLBBackendsInfo struct {
+	AddTimestamp   string   `json:"addTimestamp"`
+	InstanceName   string   `json:"instanceName"`
+	InstanceStatus int      `json:"instanceStatus"`
+	LanIP          string   `json:"lanIp"`
+	Port           int      `json:"port"`
+	UnInstanceID   string   `json:"unInstanceId"`
+	WanIPSet       []string `json:"wanIpSet"`
+	Weight         int      `json:"weight"`
+}
+
+func (client *Client) DescribeForwardLBBackends(loadBalancerId string, listenerIds []string,protocol int, loadBalancerPort int) (
+	*DescribeForwardLBBackendsResponse,
+	error,
+) {
+	args := &DescribeForwardLBBackendsArgs{
+		LoadBalancerId: loadBalancerId,
+		Listeners: listenerIds,
+		Protocol: protocol,
+		LoadBalancerPort: loadBalancerPort,
+	}
+
+	response := &DescribeForwardLBBackendsResponse{}
+	err := client.Invoke("DescribeForwardLBBackends", args, response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+type RegisterInstancesWithForwardLBFourthListenerArgs struct {
+	LoadBalancerId    string                  `qcloud_arg:"loadBalancerId,required"`
+	ListenerId        string                  `qcloud_arg:"listenerId,required"`
+	Backends       []ForwardRegisterInstancesOpts `qcloud_arg:"backends,required"`
+}
+
+type ForwardRegisterInstancesOpts struct {
+	InstanceId string `qcloud_arg:"instanceId,required"`
+	Port       *int   `qcloud_arg:"port"`
+	Weight     *int   `qcloud_arg:"weight"`
+}
+
+type RegisterInstancesWithForwardLBFourthListenerResponse struct {
+	Response
+	RequestId int `json:"requestId"`
+}
+
+func (response RegisterInstancesWithForwardLBFourthListenerResponse) Id() int {
+	return response.RequestId
+}
+
+func (client *Client) RegisterInstancesWithForwardLBFourthListener(args *RegisterInstancesWithForwardLBFourthListenerArgs) (
+	*RegisterInstancesWithForwardLBFourthListenerResponse,
+	error,
+) {
+	response := &RegisterInstancesWithForwardLBFourthListenerResponse{}
+	err := client.Invoke("RegisterInstancesWithForwardLBFourthListener", args, response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type DeregisterInstancesFromForwardLBFourthListenerArgs struct {
+	LoadBalancerId    string              `qcloud_arg:"loadBalancerId,required"`
+	ListenerId        string                  `qcloud_arg:"listenerId,required"`
+	Backends       []ForwardDeregisterInstances `qcloud_arg:"backends,required"`
+}
+
+type ForwardDeregisterInstances  struct {
+	InstanceId string `qcloud_arg:"instanceId"`
+	Port       *int   `qcloud_arg:"port"`
+}
+
+type DeregisterInstancesFromForwardLBFourthListenerResponse struct {
+	Response
+	RequestId int `json:"requestId"`
+}
+
+func (response DeregisterInstancesFromForwardLBFourthListenerResponse) Id() int {
+	return response.RequestId
+}
+
+func (client *Client) DeregisterInstancesFromForwardLBFourthListener(LoadBalancerId string, listenId string,InstanceIds []string) (
+	*DeregisterInstancesFromForwardLBFourthListenerResponse,
+	error,
+) {
+
+	backends := []ForwardDeregisterInstances{}
+	for _, instanceId := range InstanceIds {
+		backends = append(backends, ForwardDeregisterInstances{InstanceId: instanceId})
+	}
+
+	args := &DeregisterInstancesFromForwardLBFourthListenerArgs{
+		LoadBalancerId: LoadBalancerId,
+		ListenerId: listenId,
+		Backends:       backends,
+	}
+	response := &DeregisterInstancesFromForwardLBFourthListenerResponse{}
+	err := client.Invoke("DeregisterInstancesFromForwardLBFourthListener", args, response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
---
+++ clb/listener_forword.go
@@ -0,0 +1,82 @@
+package clb
+
+type DescribeForwardLBListenersArgs struct {
+	LoadBalancerId   string    `qcloud_arg:"loadBalancerId,required"`
+	ListenerIds      *[]string `qcloud_arg:"listenerIds"`
+	Protocol         *int      `qcloud_arg:"protocol"`
+	LoadBalancerPort *int32    `qcloud_arg:"loadBalancerPort"`
+}
+
+type DescribeForwardLBtenersResponse struct {
+	Response
+	TotalCount  int        `json:"totalCount"`
+	ListenerSet []ListenerForward `json:"listenerSet"`
+}
+
+type ListenerForward struct {
+	ListenerId       string `json:"listenerId"`
+	LoadBalancerPort int32  `json:"loadBalancerPort"`
+	Protocol         int    `json:"protocol"`
+	ProtocolType     string `json:"protocolType"`
+	SSLMode          string `json:"SSLMode"`
+	CertId           string `json:"certId"`
+	CertCaId         string `json:"certCaId"`
+	Rules            []ListenerForwardRule `json:"rules"`
+}
+
+type ListenerForwardRule struct {
+
+}
+
+func (client *Client) DescribeForwardLBListeners(args *DescribeForwardLBListenersArgs) (
+	*DescribeForwardLBtenersResponse,
+	error,
+) {
+	response := &DescribeForwardLBtenersResponse{}
+	err := client.Invoke("DescribeForwardLBListeners", args, response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type CreateForwardLBFourthLayerListenersArgs struct {
+	LoadBalancerId string               `qcloud_arg:"loadBalancerId,required"`
+	Listeners      []CreateForwardLBFourthLayerListenersOpts `qcloud_arg:"listeners"`
+}
+
+type CreateForwardLBFourthLayerListenersOpts struct {
+	LoadBalancerPort int32   `qcloud_arg:"loadBalancerPort,required"`
+	Protocol         int     `qcloud_arg:"protocol,required"`
+	ListenerName     *string `qcloud_arg:"listenerName"`
+	SessionExpire    *int    `qcloud_arg:"sessionExpire"`
+	HealthSwitch     *int    `qcloud_arg:"healthSwitch"`
+	TimeOut          *int    `qcloud_arg:"timeOut"`
+	IntervalTime     *int    `qcloud_arg:"intervalTime"`
+	HealthNum        *int    `qcloud_arg:"healthNum"`
+	UnhealthNum      *int    `qcloud_arg:"unhealthNum"`
+	HttpHash         *int    `qcloud_arg:"httpHash"`
+	Scheduler         *int    `qcloud_arg:"scheduler"`
+}
+
+type CreateForwardLBFourthLayerListenersResponse struct {
+	Response
+	RequestId   int      `json:"requestId"`
+	ListenerIds []string `json:"listenerIds"`
+}
+
+func (response CreateForwardLBFourthLayerListenersResponse) Id() int {
+	return response.RequestId
+}
+
+func (client *Client) CreateForwardLBFourthLayerListeners(args *CreateForwardLBFourthLayerListenersArgs) (
+	*CreateForwardLBFourthLayerListenersResponse,
+	error,
+) {
+	response := &CreateForwardLBFourthLayerListenersResponse{}
+	err := client.Invoke("CreateForwardLBFourthLayerListeners", args, response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
